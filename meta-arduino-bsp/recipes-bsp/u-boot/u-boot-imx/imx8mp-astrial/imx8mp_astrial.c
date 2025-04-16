8e011b5d0a3bf9ac89856d3a6036e489fa19af09
Massimo Pennazio <maxipenna@libero.it>
2025-04-16 12:40:18 +0200

    Rebasing against current BSP / u-boot

--- meta-arduino-bsp/recipes-bsp/u-boot/u-boot-imx/imx8mp-astrial/imx8mp_astrial.c
+++ meta-arduino-bsp/recipes-bsp/u-boot/u-boot-imx/imx8mp-astrial/imx8mp_astrial.c
@@ -27,8 +27,6 @@
 #include "../../freescale/common/tcpc.h"
 #include <usb.h>
 #include <dwc3-uboot.h>
-#include <imx_sip.h>
-#include <linux/arm-smccc.h>
 #include <mmc.h>
 
 DECLARE_GLOBAL_DATA_PTR;
@@ -395,15 +393,14 @@ int board_usb_init(int index, enum usb_init_type init)
 int board_usb_cleanup(int index, enum usb_init_type init)
 {
 	int ret = 0;
-	if (index == 0 && init == USB_INIT_DEVICE) {
+	if ((index == 0 || index == 1) && init == USB_INIT_DEVICE) {
 		dwc3_uboot_exit(index);
-	} else if (index == 0 && init == USB_INIT_HOST) {
+		imx8m_usb_power(index, false);
+	} else if ((index == 0 || index == 1) && init == USB_INIT_HOST) {
 #ifdef CONFIG_USB_TCPC
 		ret = tcpc_disable_src_vbus(&port1);
 #endif
 	}
-
-	imx8m_usb_power(index, false);
 
 	return ret;
 }
@@ -464,19 +461,10 @@ int board_phy_config(struct phy_device *phydev)
 }
 #endif
 
-#define DISPMIX				13
-#define MIPI				15
-
 int board_init(void)
 {
-	struct arm_smccc_res res;
-
 #ifdef CONFIG_USB_TCPC
 	setup_typec();
-
-	/* Enable USB power default */
-	imx8m_usb_power(0, true);
-	imx8m_usb_power(1, true);
 #endif
 
 	if (IS_ENABLED(CONFIG_FEC_MXC)) {
@@ -494,12 +482,6 @@ int board_init(void)
 #if defined(CONFIG_USB_DWC3) || defined(CONFIG_USB_XHCI_IMX8M)
 	init_usb_clk();
 #endif
-
-	/* enable the dispmix & mipi phy power domain */
-	arm_smccc_smc(IMX_SIP_GPC, IMX_SIP_GPC_PM_DOMAIN,
-		      DISPMIX, true, 0, 0, 0, 0, &res);
-	arm_smccc_smc(IMX_SIP_GPC, IMX_SIP_GPC_PM_DOMAIN,
-		      MIPI, true, 0, 0, 0, 0, &res);
 
 	return 0;
 }
